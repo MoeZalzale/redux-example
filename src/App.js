97efb3bdb3f388d77b0f8a539af692f47e80c8df
Moe Zalzale <mohamad_zalzale@hotmail.com>
2022-04-14 00:46:29 -0400

    redux example

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import {useSelector,useDispatch} from 'react-redux'
+import { increment,decrement } from './redux/counterSlice';
 
 function App() {
+
+const {value} = useSelector((state)=>state.counter)
+const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+     <h2> COunter is at {value}</h2>
+     <button onClick= {()=> dispatch(increment())}> Increment</button>
+     <button onClick= {()=> dispatch(decrement())}> decrement
+     </button>
     </div>
   );
 }
